6ca65d22b156f1a61c970f0263cc3bfbcd5c279f
Stacey Terman <seterman@mit.edu>
2015-04-13 21:57:37 -0400

    Detect and highlight intersecting buttons

--- leap-things.js
+++ leap-things.js
@@ -1,49 +1,99 @@
+var buttons;
+
+// Gets the screen position of the given Leap position
+// also flips the y value to correspond to browser coordinates
+var roundedPos = function(handMesh, pos) {
+    var newPos = handMesh.screenPosition(pos);
+    newPos.y = window.innerHeight - newPos.y;
+    return { x: Math.round(newPos.x), y: Math.round(newPos.y) };
+};
+
+var intersectingButton = function(pos) {
+    var intersecting = false;
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons.eq(i);
+        var xmin, xmax, ymin, ymax;
+        xmin = button.offset().left;
+        xmax = xmin + button.width();
+        ymin = button.offset().top;
+        ymax = ymin + button.height();
+
+        if (pos.x >= xmin && pos.x <= xmax &&
+                pos.y >= ymin && pos.y <= ymax) {
+            button.addClass('intersecting-hand');
+            intersecting = button;
+        } else {
+            button.removeClass('intersecting-hand');
+        }
+    }
+    return intersecting;
+};
+
+var screenPositionOptions = {
+    // positioning: 'absolute',
+    // function(vec3) {
+    //     console.log(vec3);
+    //     return vec3;
+    // }
+    // verticalOffset: 100,
+    // scale: 0.6
+};
+
+var riggedHandOptions = {
+    // parent: scene,
+    // camera: camera,
+    // renderer: renderer,
+    // renderFn: render,
+    // positionScale: 1.5
+    // offset: new THREE.Vector3(0, -1000, 0)
+};
+
 $(document).ready(function() {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 0);
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth - 20, window.innerHeight - 20);
-
-    var geometry = new THREE.BoxGeometry( 1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 15;
-    camera.position.y = 5;
-    camera.position.x = 0;
-    // renderer.render(scene, camera);
-
-    var render = function() {
-        // requestAnimationFrame(render);
-
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-        renderer.render(scene, camera);
-    };
-    // render();
-
-    var riggedHandOptions = {
-        parent: scene,
-        camera: camera,
-        renderer: renderer,
-        renderFn: render,
-        positionScale: 1.5
-    };
-
-    var posDiv = $('<div>');
+    // For debugging purposes:
+    var posDiv = $('<div>0').css({color: 'white', position:'absolute', top: 50, left:10});
+    var posDiv2 = $('<div>0').css({color: 'orange', position:'absolute', top:100, left:10});
+    var otherDiv = $('<div>0').css('color', 'white');
 
     var controllerOptions = {};
     var controller = Leap.loop(controllerOptions, function(frame) {
-        // if (frame.hands.length >= 1) {
-        //     var hand = frame.hands[0];
-        //     var handMesh = hand.data('riggedHand.mesh');
-        //     posDiv.text(handMesh.screenPosition(hand.fingers[1].tipPosition));
-        // }
+        var rhScope = this.plugins.riggedHand;
+        if (frame.hands.length >= 1) {
+            var hand = frame.hands[0];
+            var handMesh = hand.data('riggedHand.mesh');
+
+            var handPos = roundedPos(handMesh, hand.palmPosition);
+            // posDiv.text(hand.screenPosition(hand.palmPosition).map(Math.round));
+            posDiv.text([handPos.x, handPos.y]);
+
+            var btn = intersectingButton(handPos);
+
+
+            // var btnPos = hand.screenPosition([buttons[0].position().left, buttons[0].position().top, 0]);
+            // posDiv2.text(btnPos);
+            // posDiv2.text(hand.screenPosition([0, 0, 0]))
+            // throw new Error('stop');
+        }
     });
 
-    controller.use('screenPosition');
+    controller.use('screenPosition', screenPositionOptions);
+
+    // controller.use('boneHand', boneHandOptions);
     controller.use('riggedHand', riggedHandOptions);
-    $('body').append($(renderer.domElement));
-    $('body').append(posDiv);
+    $('body').append(posDiv, posDiv2, otherDiv);
+
+    buttons = $('.buttons').children();
+
+    // $('body').append(buttons);
+    // $('.bigBtn').css({ width: 100, height: 100, opacity: 0.5 });
+
+    // $('body').append($('<div>').css({
+    //     width: 10,
+    //     height: 10,
+    //     'background-color': 'red',
+    //     position: 'absolute',
+    //     left: window.innerWidth/2,
+    //     top: 0
+    // }));
+    // console.log(buttons[0].position())
 });
